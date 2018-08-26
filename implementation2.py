1be5a9ee4daf828f494559d41b89ab1bf6328a21
yorke991 <36922512+yorke991@users.noreply.github.com>
2018-08-26 21:00:52 +1000

    updated

--- implementation2.py
+++ implementation2.py
@@ -1,4 +1,4 @@
-pyfrom abc import ABC, abstractmethod
+from abc import ABC, abstractmethod
 class Customer(ABC):
     def __init__(self, name, email, licenseNo, age):
         self._name = name
@@ -6,24 +6,24 @@ class Customer(ABC):
         self._licenseNo = licenseNo
         self._age = age
 
-    def get_name():
+    def get_name(self):
         return self._name;
-    def set_name():
+    def set_name(name):
         self._name = name;
 
-    def get_email():
+    def get_email(self):
         return self._email;
-    def set_email():
+    def set_email(email):
         self._email = email;
 
-    def get_licenseNo():
+    def get_licenseNo(self):
         return self._licenseNo;
-    def set_licenseNo():
+    def set_licenseNo(licenseNo):
         self._licenseNo = licenseNo;
 
-    def get_age():
+    def get_age(self):
         return self._age;
-    def set_age():
+    def set_age(age):
         self._age = age;
 
     @abstractmethod
@@ -31,24 +31,25 @@ class Customer(ABC):
         pass
 
 class Order(Customer):
-    def __init__(self, start, end, totalDuration):
+    def __init__(self, name, email, licenseNo, age, start, end, totalDuration):
+        super().__init__(name, email, licenseNo, age)
         self._start = start
         self._end = end
         self._totalDuration = totalDuration
 
-    def get_start():
+    def get_start(self):
         return self._start;
-    def set_start():
+    def set_start(start):
         self._start = start;
 
-    def get_end():
+    def get_end(self):
         return self._end;
-    def set_end():
+    def set_end(end):
         self._end = end;
 
-    def get_totalDuration():
+    def get_totalDuration(self):
         return self._totalDuration;
-    def set_totalDuration():
+    def set_totalDuration(totalDuration):
         self._totalDuration = totalDuration;
 
     # implementation after merging with car
@@ -56,4 +57,14 @@ class Order(Customer):
         pass
 
     def __str__(self):
-        pass
+        return Customer.get_name(self) + " (License number: {0})".format(Customer.get_licenseNo(self)) \
+        + " has made a booking from {0}".format(self.get_start()) + " to {0}".format(self.get_end())
+
+
+# main function
+print("-----Book a car form-----")
+print("Please enter customer details:")
+customer1 = Order(input("Name: "), input("Email: "), input("License Number: "), input("Age: "),
+input("Booking start date (DDMMYYYY): "), input("Booking end date (DDMMYYYY): "), input("Total booking length (days): "))
+print(customer1)
+
